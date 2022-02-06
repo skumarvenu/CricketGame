6437eb54b5aca81633d0b80f542275b5c83050c9
skumarvenu <skumarvenu@gmail.com>
2022-02-06 15:23:14 +0530

    Phase 1 : Game of Cricket

--- src/cricketgame/MatchController.java
+++ src/cricketgame/MatchController.java
@@ -1,12 +1,10 @@
 package cricketgame;
 
-import java.util.List;
+
 
 public class MatchController {
   public void start() {
     Match match = new Match();
     match.play();
   }
-
-  public void stop() {}
 }
